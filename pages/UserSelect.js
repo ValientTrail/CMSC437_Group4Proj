7479906497a5d3b95158d0651ae8f9e02b207ada
Jabril Hall <jabril6187@gmail.com>
2024-06-28 00:23:04 -0400

    Added cool buttion component along with creating UserSelect Page

---
+++ pages/UserSelect.js
@@ -0,0 +1,29 @@
+class UserSelect extends React.Component {
+    constructor(props){
+        super(props)
+        this.handlePatientClick = this.handlePatientClick.bind(this);
+    }
+    handlePatientClick(){
+        console.log("clicked!")
+    }
+
+    render(){
+        return(
+            <div style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                <WifiBar/>
+                <StyledButton 
+                    onClick = { this.handlePatientClick }
+                    style = {{
+                        width: "400px",
+                    }}
+                />
+            </div>
+        )
+    }
+}
