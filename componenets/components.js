9d4fd5e9c68d9877de985f572c288a2e664281d2
Jabril Hall <jabril6187@gmail.com>
2024-06-27 23:25:14 -0400

    Made components one file to save space

---
+++ componenets/components.js
@@ -0,0 +1,51 @@
+class WifiBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.LTE = "./images/LTE.png";
+    }
+
+    render(){
+
+        return (
+        <div className="wb-Background">
+            <div className="battery-background">
+                <div className="battery" style={{width:"70%"}}></div>
+            </div>
+            <img src={this.LTE} alt="LTE" style={{
+                position: "absolute",
+                left: "45%",
+                top: "12%",
+                width: "60px",
+                height: "32px"
+            }}></img>
+            <h1 style={{    
+                position: "absolute",
+                right: "5%",
+                bottom: "-25%"}}
+                >10:09</h1>
+            
+        </div>
+    )
+}
+}
+
+
+class AppBackground extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            page: props.page
+        };
+        console.log(props);
+    }
+
+    render(){return(
+        <div className="appFrame">
+            <div className="appBackground">
+                {
+                    this.state.page
+                }
+            </div>
+        </div>
+    )}
+}
