c0eed3d126d5ba62669eb692672327c60b54e7e1
Jabril Hall <jabril6187@gmail.com>
2024-06-28 22:09:29 -0400

    added tileable heartrate monitor

---
+++ pages/PatientInfo.js
@@ -0,0 +1,24 @@
+class PatientInfoScreen extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            style: props.style ?? {},
+                        
+        }
+    }
+
+    render(){
+        return(
+            <div style={{
+                display:"flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+                height: "100%",
+            }}>
+
+                <HeartRateGraph />
+            </div>
+        )
+    }
+} 
