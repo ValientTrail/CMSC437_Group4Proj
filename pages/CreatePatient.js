01ae5b0b9c6a6b3ecfb28db5709efdf8940b7bb7
FelicityBlue <zinfelicity@gmail.com>
2024-07-04 06:03:48 -0400

    remove checkbox medical history and add textbox for info instead

--- pages/CreatePatient.js
+++ pages/CreatePatient.js
@@ -22,66 +22,92 @@ class CreatePatient extends React.Component {
           <nav>
             <form id="patient-info-form">
               <div className="patient-info-input">
-                <div className="input">
-                  <label>First Name: </label>
-                  <input type="text" id="fname" name="fname" />
-                </div>
-                <div className="input">
-                  <label>Last Name: </label>
-                  <input type="text" id="lname" name="lname" />
-                </div>
-                <div id="sex-choice">
-                  <label>Sex: </label>
-                  <input type="radio" id="choice-male" name="choice"></input>
-                  <label htmlFor="choice-male">Male</label>
-                  <input type="radio" id="choice-female" name="choice"></input>
-                  <label htmlFor="choice-female">Female</label>
-                </div>
+                {/* Patient Basic Info */}
+                <nav className="container">
+                  {/* Patient's Name */}
+                  <nav>
+                    <div className="input">
+                      <label>First Name: </label>
+                      <input type="text" id="fname" name="fname" />
+                    </div>
+                    <div className="input">
+                      <label>Last Name: </label>
+                      <input type="text" id="lname" name="lname" />
+                    </div>
+                  </nav>
 
+                  <nav>
+                    {/* Patient's Age */}
+                    <div className="input">
+                      <label>BirthDate: </label>
+                      <input type="text" id="age" name="age" />
+                    </div>
+                    {/* Patient's Sex */}
+                    <div id="sex-choice">
+                      <label>Sex: </label>
+                      <input
+                        type="radio"
+                        id="choice-male"
+                        name="choice"
+                      ></input>
+                      <label htmlFor="choice-male">Male</label>
+                      <input
+                        type="radio"
+                        id="choice-female"
+                        name="choice"
+                      ></input>
+                      <label htmlFor="choice-female">Female</label>
+                    </div>
+                  </nav>
+                </nav>
+
+                {/* Patient's Medical Information */}
                 <div id="medical-history" className="container">
                   <label>Medical History: </label>
-                  <nav className="container-style">
-                    <input type="checkbox" id="diabete-1"></input>
-                    <label htmlFor="diabete-1">Type 1-Diabetes</label>
-                  </nav>
-                  <nav className="container-style">
-                    <input type="checkbox" id="diabete-2"></input>
-                    <label htmlFor="diabete-1">Type 2-Diabetes</label>
-                  </nav>
-                  <nav className="container-style">
-                    <input type="checkbox" id="cardiac"></input>
-                    <label htmlFor="cardiac">Cardiac</label>
-                  </nav>
-                  <nav className="container-style">
-                    <input type="checkbox" id="respiratory"></input>
-                    <label htmlFor="respiratory">Respiratory</label>
-                  </nav>
-                  <nav className="container-style">
-                    <input type="checkbox" id="allergies"></input>
-                    <label htmlFor="allergies">Allergies</label>
-                  </nav>
-                  <nav className="container-style">
-                    <input type="checkbox" id="medications"></input>
-                    <label htmlFor="medications">Medications</label>
-                  </nav>
-                  <nav className="container-style">
-                    <input type="checkbox" id="injuries"></input>
-                    <label htmlFor="injuries">Injuries</label>
-                  </nav>
                   <br />
-                  <nav className="container-style">
-                    <input type="checkbox" id="other"></input>
-                    <label htmlFor="other">Other</label>
-                    <textarea id="data-entry" rows="2" cols="20"></textarea>
-                  </nav>
+                  <textarea id="data-entry" rows="2" cols="55"></textarea>
                 </div>
+                <div className="container">
+                  <label>Incident Report: </label>
+                  <br />
+                  <textarea id="data-entry" rows="3" cols="55"></textarea>
+                </div>
+
+                {/* Patient's Contact Information */}
+                <div>
+                  <label>Address</label>
+                </div>
+                <nav className="container">
+                  <div className="input">
+                    <label>Street: </label>
+                    <input type="text" />
+                  </div>
+                  <div className="input">
+                    <label>City: </label>
+                    <input type="text" />
+                  </div>
+                </nav>
+                <nav className="container">
+                  <div className="input">
+                    <label>State: </label>
+                    <input type="text" />
+                  </div>
+                  <div className="input">
+                    <label>Zipcode: </label>
+                    <input type="number" />
+                  </div>
+                </nav>
+                <div className="input">
+                  <label>Phone Number: </label>
+                  <input type="tel" />
+                </div>
+                {/* Submit Button */}
+                <nav id="submit-button">
+                  <button type="submit">CREATE</button>
+                </nav>
               </div>
             </form>
-            <nav id="submit-button">
-              <button type="submit">
-                CREATE
-              </button>
-            </nav>
+            
           </nav>
         </nav>
       </div>
